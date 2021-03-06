9e26969c3b89ce2148717fdcbddc5857d2a2deae
AppleHD <kartik3rde@gmail.com>
2021-03-06 19:15:52 +0530

    Updates

---
+++ precache-manifest.77fd8ff8e65df6a26991ebd516adec86.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e4774697397b9931aa18872b4da0cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efab63a45deda7a06325",
+    "url": "/static/css/main.7482fb46.chunk.css"
+  },
+  {
+    "revision": "21f41281592176eda10c",
+    "url": "/static/js/2.8659357a.chunk.js"
+  },
+  {
+    "revision": "75304a605f9c03e664948edc1c5186bb",
+    "url": "/static/js/2.8659357a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efab63a45deda7a06325",
+    "url": "/static/js/main.8858603a.chunk.js"
+  },
+  {
+    "revision": "820d9b6fff880beacd76",
+    "url": "/static/js/runtime-main.7a85cd74.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
